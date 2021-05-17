5b0fbbde0cd00ee6fe4a51047fadd956f0370cca
erdeszt <erdeszt@gmail.com>
2021-05-17 16:42:30 +0200

    Get the proxy and prime servers to communicate and change response to stream

--- prime-server/src/main/scala/primes/Main.scala
+++ prime-server/src/main/scala/primes/Main.scala
@@ -1,9 +1,21 @@
 package primes
 
+import primes.protocol.get.ZioGet._
 import zio._
+import io.grpc.Status
+import primes.protocol.get.{GetPrimesRequest, GetPrimesResponse}
+import zio.stream.ZStream
+import scalapb.zio_grpc.ServerMain
+import scalapb.zio_grpc.ServiceList
 
-object Main extends zio.App {
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    console.putStrLn("Prime server").exitCode
+object PrimesImpl extends ZPrimes[ZEnv, Any] {
+
+  override def getPrimes(request: GetPrimesRequest): ZStream[ZEnv, Status, GetPrimesResponse] = {
+    ZStream.fromIterable(List(2, 3, 5, 7, 11, 13).map(GetPrimesResponse(_)))
   }
+
+}
+
+object Main extends ServerMain {
+  def services: ServiceList[zio.ZEnv] = ServiceList.add(PrimesImpl)
 }
--- proxy-server/src/main/scala/primes/Main.scala
+++ proxy-server/src/main/scala/primes/Main.scala
@@ -1,9 +1,24 @@
 package primes
 
+import primes.protocol.get.GetPrimesRequest
+import primes.protocol.get.ZioGet._
 import zio._
+import io.grpc.ManagedChannelBuilder
+import scalapb.zio_grpc.ZManagedChannel
 
 object Main extends zio.App {
+
+  val clientLayer = PrimesClient.live(
+    ZManagedChannel(
+      ManagedChannelBuilder.forAddress("127.0.0.1", 9000).usePlaintext()
+    )
+  )
+
+  val app = PrimesClient
+    .getPrimes(GetPrimesRequest(10))
+    .foreach(prime => console.putStrLn(s"Prime: ${prime}"))
+
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    console.putStrLn("Prime server").exitCode
+    app.provideSomeLayer[zio.ZEnv](clientLayer).exitCode
   }
 }
